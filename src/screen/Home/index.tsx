b7c485e35ca27def8983b1daf2446772f88a647d
Amina <amina@techservicesstudio.com>
2025-09-13 18:01:55 +0500

    feat: Add category selection buttons in Home component

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -4,11 +4,12 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import HomeHeader from './HomeHeader';
 import fonts from '../../utils/fonts';
-import { cards } from '../../constants/data'
+import { cards, categories } from '../../constants/data'
 import HomeSlider from './HomeSlider';
 
 const Home: React.FC = () => {
@@ -31,6 +32,29 @@ const Home: React.FC = () => {
           <View style={styles.titleContainer}>
             <Text style={styles.h2}>Our Recommendation</Text>
             <Text style={styles.seeAll}>See All</Text>
+          </View>
+          <View style={styles.categoriesContainer}>
+            <TouchableOpacity
+              style={[styles.category, { backgroundColor: '#FF8000' }]}
+            >
+              <Text style={styles.SelectedCategoryText}>All</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[styles.category, { backgroundColor: '#FF80000A' }]}
+            >
+              <Text style={styles.categoryText}>Villa</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.category, { backgroundColor: '#FF80000A' }]}
+            >
+              <Text style={styles.categoryText}>Apartments</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.category, { backgroundColor: '#FF80000A' }]}
+            >
+              <Text style={styles.categoryText}>Other</Text>
+            </TouchableOpacity>
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
@@ -60,6 +84,35 @@ const styles = StyleSheet.create({
     fontFamily: fonts.SemiBold,
     fontWeight: 600,
   },
+  category: {
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: '#FF80001A',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 12,
+    alignSelf: 'flex-start', 
+  },
+
+  categoryText: {
+    color: '#191D31',
+    fontSize: 14,
+    fontFamily: fonts.Regular,
+    fontWeight: 400,
+  },
+  SelectedCategoryText: {
+    color: '#FFFFFF',
+    fontSize: 14,
+    fontFamily: fonts.SemiBold,
+    fontWeight: 600,
+  },
+  categoriesContainer: {
+    marginLeft: 8,
+    marginVertical: 24,
+    flexDirection:'row'
+  }
 });
 
 export default Home;
